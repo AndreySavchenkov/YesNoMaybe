56e6924a78eceab7b10727c43aca891818c4e9fd
sandvik16 <sannndvik16@gmail.com>
2022-01-01 16:45:55 +0300

    add component Typography from MaterialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 import {answerAPI} from "./api/api";
-import {Button, Container, Stack} from "@mui/material";
+import {Button, Container, Paper, Stack, Tooltip, Typography} from "@mui/material";
+import ThumbsUpDownIcon from '@mui/icons-material/ThumbsUpDown';
+
+
 
 function App() {
 
     const [image, setImage] = useState('')
 
-
     const getAnswer = async () => {
         const answer = await answerAPI.ask()
         setImage(answer.data.image)
     }
-
 
     return (
         <Container>
@@ -20,8 +21,12 @@ function App() {
                    justifyContent="center"
                    alignItems="center"
                    spacing={5}>
-                <h1>Нужен знак свышe?</h1>
-                <Button onClick={getAnswer} variant="contained">Ну жми</Button>
+                <Typography mt={2} variant="h5" component="h5">
+                    <ThumbsUpDownIcon/> Нужен знак свышe?
+                </Typography>
+                <Tooltip title='Нажми для получения ответа с сервера'>
+                    <Button onClick={getAnswer} variant="contained">Ну жми</Button>
+                </Tooltip>
                 <div>
                     <img src={image} alt=""/>
                 </div>
